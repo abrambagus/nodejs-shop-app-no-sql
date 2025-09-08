b5874e61fb7aee46658625ff95f2ca8cb79b919e
abram <abrambagus@gmail.com>
2025-09-08 17:13:35 +0700

    connect database with mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://abrambagus_db_user:Le8JaOB7qFK7b175@cluster0.d1jikx7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((result) => {
+      console.log("Connected to MongoDB");
+      callback(result);
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
