f144296ed23a37318bd292fcf22e2fcb2cbf1a32
abram <abrambagus@gmail.com>
2025-09-09 10:09:07 +0700

    connecting to the database

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://abrambagus_db_user:Le8JaOB7qFK7b175@cluster0.d1jikx7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://abrambagus_db_user:Le8JaOB7qFK7b175@cluster0.d1jikx7.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
     .then((result) => {
       console.log("Connected to MongoDB");
-      callback(result);
+      _db = result.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
