99d2dad1123230138f3f784f13d0244e9d453699
abram <abrambagus@gmail.com>
2025-09-12 14:05:31 +0700

    add renational order datan & get order

--- models/user.js
+++ models/user.js
@@ -74,19 +74,6 @@ class User {
       );
   }
 
-  addOrder() {
-    const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
-      .then(() => {
-        this.cart = { items: [] };
-        return db
-          .collection("users")
-          .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } });
-      });
-  }
-
   static findById(userId) {
     const db = getDb();
     return db
@@ -99,6 +86,32 @@ class User {
       .catch((err) => {
         console.log(err);
       });
+  }
+
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: this._id,
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then(() => {
+        this.cart = { items: [] };
+        return db
+          .collection("users")
+          .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } });
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db.collection("orders").find({ "user._id": this._id }).toArray();
   }
 }
 
